312adaf78d500b6f2aa5150b197419f295189037
chris <cregenye@localhost.localdomain>
2018-08-08 13:30:52 +0000

    .

--- peatio_install.sh
+++ peatio_install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sudo useradd -c "Peatio install user" --groups sudo --shell /bin/bash --create-home peatio 
-sudo echo "peatio ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers
+sudo sh -c "echo 'peatio ALL=(ALL:ALL) NOPASSWD: ALL' >> /etc/sudoers"
+sudo sh -c "sed -i '/^127/ s/$/ '$(hostname)'/' /etc/hosts"
 sudo cp ./pi.sh /home/peatio
-sudo -u peatio /home/pi.sh
+sudo -u peatio /home/peatio/pi.sh
