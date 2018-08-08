26d150b2e298ddbc10e4dc5fd39fe24a8873667b
chris <cregenye@localhost.localdomain>
2018-08-08 14:08:18 +0000

    run pi.sh in a peatio login shell

--- peatio_install.sh
+++ peatio_install.sh
@@ -4,4 +4,4 @@ sudo useradd -c "Peatio install user" --groups sudo --shell /bin/bash --create-h
 sudo sh -c "echo 'peatio ALL=(ALL:ALL) NOPASSWD: ALL' >> /etc/sudoers"
 sudo sh -c "sed -i '/^127/ s/$/ '$(hostname)'/' /etc/hosts"
 sudo cp ./pi.sh /home/peatio
-sudo -u peatio sh -c "/home/peatio/pi.sh"
+sudo -iu peatio sh -c "/home/peatio/pi.sh"
