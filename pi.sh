22589218fb92b72d1d3f3d2122473690065dc4f7
Chris Regenye <chris.regenye@achieve3000.com>
2018-08-23 15:03:27 -0400

    2.5.1 tweaks

--- pi.sh
+++ pi.sh
@@ -12,7 +12,10 @@ install_ruby () {
         --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 \
                     7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 
-    curl -sSL https://get.rvm.io | bash -s stable --ruby=2.5.1 --gems=rails
+    command curl -sSL https://rvm.io/mpapis.asc | sudo gpg --import -       # ruby 2.5.1
+    sudo chown peatio .gnupg/trustdb.gpg                                    # ruby 2.5.1
+    curl -sSL https://get.rvm.io | bash -s stable --ruby=2.5.1 --gems=rails # ruby 2.5.1
+    #curl -sSL https://get.rvm.io | bash -s stable --ruby=2.5.0 --gems=rails 
 
     echo "gem: --no-ri --no-rdoc" > ~/.gemrc
 
